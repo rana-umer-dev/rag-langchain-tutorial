ad5b8b881311e0c7dc4d19ab51c8a167b0eb4cd4
rana-umer-dev <umer01052@gmail.com>
2025-05-16 21:44:21 +0500

    uploaded all files

--- LAb2/task01.py
+++ LAb2/task01.py
@@ -1,3 +1,5 @@
+
+# Load Data form Research Paper
 from langchain_community.document_loaders import ArxivLoader
 
 loader = ArxivLoader(query="2103.00020", load_max_docs=1)
--- LAb2/task02.py
+++ LAb2/task02.py
@@ -1,3 +1,7 @@
+#This loader uses the pypdfium2 library to extract text from PDF files. It can sometimes handle files better than other loaders like PyPDFLoader or PDFMinerLoader.
+#Good for extracting text from scanned PDFs or images inside PDFs.
+
+
 from langchain_community.document_loaders import PyPDFium2Loader
 
 loader = PyPDFium2Loader("sample.pdf")
